1bc528304ce077109ee2e38b539ef9ac46e80cc4
Noeljunior <liamgliam@gmail.com>
2015-11-23 11:06:17 +0000

    init zmodn vendor

---
+++ configs/common.mk
@@ -0,0 +1,17 @@
+# Generic product
+PRODUCT_NAME := zmodn
+PRODUCT_BRAND := zmodn
+PRODUCT_DEVICE := generic
+
+
+
+#Version details
+PRODUCT_VERSION_MAJOR = 0
+PRODUCT_VERSION_MINOR = 1
+
+ZMODN_VERSION := ZmodN-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)
+
+# Write ZmodN into to build.prop
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.zmodn.version=$(ZMODN_VERSION)
+
---
+++ products/AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/zmodn_tuna.mk \
